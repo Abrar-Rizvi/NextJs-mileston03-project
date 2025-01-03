d1c7b7b78d68d28fd1bf5be10dc66dbe35447a82
Muhammad Abrar <abrar8949@gmail.com>
2025-01-03 09:41:52 -0800

    update code

--- src/app/context/context.tsx
+++ src/app/context/context.tsx
@@ -1,19 +1,29 @@
 "use client"
+import { IProduct } from '@/Data';
 import { createContext, } from 'react'
 
 
 
-interface Count {
-    [x: string]: any | number
-}
+// interface Count {
+//     [x: string]: any | number
+// }
 
 // interface Data {
 //     name: string;
 //     count: number;
 //   }
+interface CountContextType {
+    count: number;
+    setCount: React.Dispatch<React.SetStateAction<number>>; // Function to update `count`
+  }
+  
+  interface DataContextType {
+    data: IProduct | null;
+    setData: React.Dispatch<React.SetStateAction<IProduct | null>>; // Function to update `data`
+  }
 
 
 
-export const countContext = createContext({} as Count )
-export const dataContext = createContext({} as Count)
+export const countContext = createContext({} as  CountContextType  )
+export const dataContext = createContext({} as DataContextType)
 
